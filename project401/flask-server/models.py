c9f0f5347d02c2d629d9e56c5d9283b5efbb3081
Jessie Yang <jiaxiyan@usc.edu>
2022-11-12 17:06:41 -0800

    add endpoints

--- project401/flask-server/models.py
+++ project401/flask-server/models.py
@@ -1,5 +1,5 @@
 from neo4j import GraphDatabase
-
+import json
 
 class Transaction:
     def __init__(self, uri, user, password):
@@ -9,6 +9,17 @@ class Transaction:
     def close(self):
         print("close")
         self.driver.close()
+
+    def pass_cypher(self, cypher):
+        with self.driver.session() as session:
+            greeting = session.execute_write(self._pass_cypher, cypher)
+            return greeting
+
+    @staticmethod
+    def _pass_cypher(tx, cypher):
+        print(cypher)
+        result = tx.run(cypher)
+        return result.peek()
 
     def load_csv(self):
         print("load csv")
@@ -28,9 +39,39 @@ class Transaction:
 
     def read_transactions(self):
         with self.driver.session() as session:
-            transactions = session.execute_read(self._get_transactions)
-            # for record in transactions:
-            #     print(record["n"])
+            return session.execute_read(self._get_transactions)
+
+    # def create_guest_node(session):
+    #     insert_query_guest = '''
+    #     MATCH (a:From_address)
+    #     WITH collect({add: a.add, nodeType:'from'}) AS nodes RETURN nodes
+    #     '''
+
+    #     result = session.run(insert_query_guest)
+    #     for record in result:
+    #         guest_node = json.dumps(dict(record))
+    #         return guest_node
+    
+    # def create_guest_edge(session):
+    #     # fetches the guest-event edges from neo4j
+    #     insert_query_guest = '''
+    #     MATCH (a:From_address)-[r:SEND_TO]->(b:To_address)
+    #     WITH collect({source: a.add, target: b.add, value:r.value}) AS edges RETURN edges
+    #     '''
+    #     result = session.run(insert_query_guest)
+    #     for record in result:
+    #         return json.dumps(dict(record))
+    
+    # def create_event_node(session):
+    #     # fetches the event nodes from neo4j
+    #     insert_query_guest = '''
+    #     MATCH (a:To_address)
+    #     WITH collect({add: a.add, nodeType:'to'}) AS nodes RETURN nodes
+    #     '''
+
+    #     result = session.run(insert_query_guest)
+    #     for record in result:
+    #         return json.dumps(record['nodes'])
 
     @staticmethod
     def _get_transactions(tx):
@@ -43,4 +84,52 @@ class Transaction:
         print(peek["n"])
         print(peek["m"])
         print(peek["r"])
+
+        # return graph json
+        insert_query_guest = '''
+        MATCH (a:From_address)
+        WITH collect({address: a.add, nodeType:'from'}) AS nodes RETURN nodes
+        '''
+        result = tx.run(insert_query_guest)
+        for record in result:
+            guest_node = json.dumps(dict(record))
+            break
+        guest_nodes = str(guest_node)[1:][:-2]
+
+        insert_query_guest = '''
+        MATCH (a:From_address)-[r:SEND_TO]->(b:To_address)
+        WITH collect({source: a.add, target: b.add, caption:r.value}) AS edges RETURN edges
+        '''
+        result = tx.run(insert_query_guest)
+        for record in result:
+            create_guest_edge = json.dumps(dict(record))
+            break
+        guest_edges = str(create_guest_edge)[1:]
+
+        insert_query_guest = '''
+        MATCH (a:To_address)
+        WITH collect({address: a.add, nodeType:'to'}) AS nodes RETURN nodes
+        '''
+        result = tx.run(insert_query_guest)
+        for record in result:
+            create_event_node = json.dumps(record['nodes'])
+            break
+
+        event_node = str(create_event_node) + ']'
+
+        graphjson = str(guest_nodes) + ', ' + str(event_node) + ',' + str(guest_edges)
+
+        print(graphjson)
+
+        return graphjson
+    
+    def get_from_address(self, fromAddress):
+        with self.driver.session() as session:
+            transactions = session.execute_read(self._get_from_address, fromAddress)
+
+    @staticmethod
+    def _get_from_address(tx, fromAddress):
+        result = tx.run("MATCH (From_address {add: $fromAddress})-[r:SEND_TO]->(To_address) "
+                        "RETURN r.value, To_address.add", fromAddress=fromAddress)
         return result.values()
+    
--- project401/flask-server/server.py
+++ project401/flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from neo4j import GraphDatabase, basic_auth
 import logging
 from neo4j.exceptions import ServiceUnavailable
@@ -7,27 +7,44 @@ from models import Transaction
 app = Flask(__name__)
 
 
-@app.route("/test")
-def try_func():
-    print("try func")
+@app.route("/load")
+def load_func():
     uri1 = "neo4j+s://34a4cf44.databases.neo4j.io"
     user1 = "neo4j"
     password1 = "Eds0XFkwVJGVvRuE8U9D907u1KQiLWVfj14L6ynD7iA"
     trans = Transaction(uri1, user1, password1)
     trans.load_csv()
-    trans.read_transactions()
     trans.close()
-    return {"members": ["1", "2", "3"]}
+    return '''
+                  load csv successfully'''
 
+@app.route("/transactions")
+def transaction_func():
+    uri1 = "neo4j+s://34a4cf44.databases.neo4j.io"
+    user1 = "neo4j"
+    password1 = "Eds0XFkwVJGVvRuE8U9D907u1KQiLWVfj14L6ynD7iA"
+    trans = Transaction(uri1, user1, password1)
+    return trans.read_transactions()
+
+@app.route("/cypher", methods=['GET', 'POST'])
+def cypher_func():
+    if request.method == 'POST':
+        cypher = request.form.get('statement')
+        uri1 = "neo4j+s://34a4cf44.databases.neo4j.io"
+        user1 = "neo4j"
+        password1 = "Eds0XFkwVJGVvRuE8U9D907u1KQiLWVfj14L6ynD7iA"
+        trans = Transaction(uri1, user1, password1)
+        result = trans.pass_cypher(cypher)
+        print(result)
+        return '''
+                  <h1>The statement is: {}</h1>'''.format(cypher)
+    return '''
+              <form method="POST">
+                  <div><label>Cypher: <input type="text" name="statement"></label></div>
+                  <input type="submit" value="Submit">
+              </form>'''
 
 if __name__ == "__main__":
-    # uri = "neo4j+s://34a4cf44.databases.neo4j.io"
-    # user = "neo4j"
-    # password = "Eds0XFkwVJGVvRuE8U9D907u1KQiLWVfj14L6ynD7iA"
-    # trans = Transaction(uri, user, password)
-    # trans.load_csv()
-    # trans.read_transactions()
-    # trans.close()
     app.run(port=8000, debug=True)
 
 
